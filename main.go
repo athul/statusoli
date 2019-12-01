b0b174078136ff196a0b68614aa7213e7bbadf1f
Athul Cyriac Ajay <athul8720@gmail.com>
2019-12-01 09:09:51 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,41 +2,41 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/yanzay/tbot/v2"
 )
-func main(){
-	icons:=map[string]string{
-		"failure":"‼️‼️ ",
-		"cancelled":"⚠️⚠️ ",
-		"success":"💯✅ ",
+
+func main() {
+	icons := map[string]string{
+		"failure":   "‼️‼️ ",
+		"cancelled": "⚠️⚠️ ",
+		"success":   "💯✅ ",
 	}
-  text:=map[string]string{
-    "failure":"Eda Mone Nee Pettu",
-    "cancelled":"Enthupatti Babymole?",
-    "success": "Adipoli Monuse",
-    }
-	var(
-		token=os.Getenv("TG_TOKEN")
-		chat=os.Getenv("CHAT_TOKEN")
-		status=os.Getenv("INPUT_STATUS")
+	text := map[string]string{
+		"failure":   "Eda Mone Nee Pettu",
+		"cancelled": "Enthupatti Babymole?",
+		"success":   "Adipoli Monuse",
+	}
+	var (
+		token  = os.Getenv("TG_TOKEN")
+		chat   = os.Getenv("CHAT_TOKEN")
+		status = os.Getenv("INPUT_STATUS")
 
-		workflow=os.Getenv("GITHUB_WORKFLOW")
-		repo=os.Getenv("GITHUB_REPOSITORY")
-		commit=os.Getenv("GITHUB_SHA")
+		workflow = os.Getenv("GITHUB_WORKFLOW")
+		repo     = os.Getenv("GITHUB_REPOSITORY")
+		commit   = os.Getenv("GITHUB_SHA")
 	)
-	c:=tbot.NewClient(token,http.DefaultClient,"https://api.telegram.org")
-	icon:=icons[strings.ToLower(status)]
-	link:=fmt.Sprintf("https://github.com/%s/commit/%s/checks",repo,commit)
+	c := tbot.NewClient(token, http.DefaultClient, "https://api.telegram.org")
+	icon := icons[strings.ToLower(status)]
+	link := fmt.Sprintf("https://github.com/%s/commit/%s/checks", repo, commit)
 
-	msg:=fmt.Sprintf(`%s*%s*: %s ([%s](%s))
-			  **%s**`, icon, status, repo, workflow, link, text)
+	msg := fmt.Sprintf(`%s*%s*: %s ([%s](%s))    **%s**`, icon, status, repo, workflow, link, text)
 
-	_, err:=c.SendMessage(chat,msg,tbot.OptParseModeMarkdown)
+	_, err := c.SendMessage(chat, msg, tbot.OptParseModeMarkdown)
 	if err != nil {
 		log.Fatalf("unable to send message: %v", err)
 	}
